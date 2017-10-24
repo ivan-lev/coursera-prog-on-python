43ee76d41486e28c22cfaa3d4d3f92f86f8832cd
Ivan Lev <fanatos@mail.ru>
2017-10-24 15:35:36 +0500

    Added printing a values respectively if we get a key only

--- week02_homework_storage.py
+++ week02_homework_storage.py
@@ -15,8 +15,25 @@ my_value = args.value
 print(my_key)
 print(my_value)
 
-#print(args.echo)
-#print(args.square**2)
+if my_value == None:
+    with open('storage.txt', 'r') as f:
+        database = {}
+        for line in f:                      #считываем построчно файл и разделяем его
+            temp_line = line.split()
+            if temp_line[0] not in database: #если ключа нет в словаре database - добавляем его списком
+                database[temp_line[0]] = [temp_line[1]]
+            else:
+                new_value = database.get(temp_line[0])  #иначе дергаем список, добавляем туда value 
+                new_value.append(temp_line[1])          #и перезаписываем список по ключу
+                database[temp_line[0]] = new_value
+        value_list = database.get(my_key)
+        for value in value_list:
+            print(value, end = ' ')
+
+else:
+    # заносим в хранилище данные по ключу
+    pass
+
 
 #storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 #with open(storage_path, 'w') as f:
