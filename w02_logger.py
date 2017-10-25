4a3e2694053e8a2ec750593972647a720d7732a0
Ivan Lev <fanatos@mail.ru>
2017-10-25 19:33:06 +0500

    More complex version of function logger

--- w02_logger.py
+++ w02_logger.py
@@ -1,14 +1,16 @@
-def logger(func):
-    def wrapped(num_list):
-        result = func(num_list)
-        with open('file.txt', 'w') as f:
-            f.write(str(result))
-        return result
-    return wrapped
+def logger(filename):
+    def decorator(func):
+        def wrapped(*args, **kwargs):
+            result = func(*args, **kwargs)
+            with open(filenamet', 'w') as f:
+                f.write(str(result))
+            return result
+        return wrapped
+    return decorator
 
-num_list = [1, 2, 3, 4 , 5]
+num_list = [1, 2, 3, 4, 5]
 
-@logger
+@logger('log_file.txt')
 def summator(num_list):
     return sum(num_list)
 
