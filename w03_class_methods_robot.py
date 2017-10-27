97672cab2206ad0cb1f974258f1bd2b948fcf815
Ivan Lev <fanatos@mail.ru>
2017-10-27 08:57:51 +0500

    Program fixed

--- w03_class_methods_robot.py
+++ w03_class_methods_robot.py
@@ -2,26 +2,31 @@ class Robot:
     def __init__(self, power):
         self._power = power
 
-        power = property()
+    power = property()
 
-        @power.setter
-        def power(self, value):
-            if value < 0:
-                self._power = 0
-            else:
-                self._power = value
+    @power.setter
+    def power(self, value):
+        if value < 0:
+            self._power = 0
+            print('Power can not be less than 0. Setting power to 0')
+        else:
+            self._power = value
 
-        @power.getter
-        def power(self):
-            return self._power
+    @power.getter
+    def power(self):
+        return self._power
 
-        @power.deleter
-        def power(self):
-            print('Powerless robot is useless robot')
-            del self._power
+    @power.deleter
+    def power(self):
+        print('Powerless robot is useless robot')
+        del self._power
 
 
-#example
+#examples
 robot = Robot(120)
 robot.power = -20
+print(robot.power)
+
+print()
+robot_2 = Robot(-120)
 print(robot.power)
