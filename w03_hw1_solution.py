bced4485c80b9d2b1e827e94f5d28342185e550b
ivan-lev <fanatos@mail.ru>
2017-11-02 15:21:43 +0500

    Week 03 little homework number 1

---
+++ w03_hw1_solution.py
@@ -0,0 +1,16 @@
+﻿import os
+
+class FileReader:   
+   def __init__(self, file_path):
+        self.file_path = file_path
+
+   def read(self):
+      try:
+         with open(self.file_path, 'r') as f:
+               return f.read()
+      except IOError:
+         print('Файла не существует')
+         return ''
+
+reader = FileReader("example1.txt")
+print(reader.read())
