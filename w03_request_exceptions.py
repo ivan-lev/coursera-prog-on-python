3ebe25df4ed15c451d1adaa62c833caaea1dbc07
ivan-lev <fanatos@mail.ru>
2017-11-02 15:21:15 +0500

    Week 03 request exceptions lesson

---
+++ w03_request_exceptions.py
@@ -0,0 +1,17 @@
+import sys
+import requests
+
+url = sys.argv[1]
+
+try:
+    response = requests.get(url, timeout = 30)
+    response.raise_for_status()
+except requests.Timeout:
+    print('ошибка timeout, url:', url)
+except requests.HTTPError as err:
+    code = err.response.status_code
+    print('ошибка url: {}, code: {}'.format(url, code))
+except requests.RequestException:
+    print('ошибка скачивания url:', url)
+else:
+    print(response.content)
